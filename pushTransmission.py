fb5dcfe5777501767377dfcb78d070d038e2fe84
Arno Hautala <arno@alum.wpi.edu>
2016-03-22 20:51:53 -0400

    formatting

--- pushTransmission.py
+++ pushTransmission.py
@@ -1,50 +1,53 @@
 #!/usr/local/python/bin/python -OO
 
-import sys
-import httplib
-import urllib
-import os
 import ConfigParser
+import httplib
+import sys
+import urllib
 
-def readConf(confFile) :
-	configuration = ConfigParser.ConfigParser()
-	file = os.path.join(os.path.dirname(sys.argv[0]), confFile)
+import os
 
-	if not os.path.isfile(file):
-		sys.exit("Config file not found")
 
-	try:
-		fp = open(file, "r")
-		configuration.readfp(fp)
-		fp.close()
-	except IOError:
-		sys.exit("Config file not readable")
+def readConf(confFile):
+    configuration = ConfigParser.ConfigParser()
+    file = os.path.join(os.path.dirname(sys.argv[0]), confFile)
 
-	return configuration
+    if not os.path.isfile(file):
+        sys.exit("Config file not found")
 
-def push(torrentName) :
-	configuration = readConf("pushover.cfg")
-	apiToken = configuration.get("Pushover", "api_token")
-	userKey = configuration.get("Pushover", "user_key")
+    try:
+        fp = open(file, "r")
+        configuration.readfp(fp)
+        fp.close()
+    except IOError:
+        sys.exit("Config file not readable")
 
-	title = "Download complete"
-	message = torrentName
+    return configuration
 
-	cx = httplib.HTTPSConnection("api.pushover.net:443")
-	cx.request(
-		"POST",
-		"/1/messages",
-		urllib.urlencode({
-			"token": apiToken,
-			"user": userKey,
-			"title": title,
-			"message": message
-			}),
-		{"Content-type": "application/x-www-form-urlencoded"}
-	)
-	
+
+def push(torrentName):
+    configuration = readConf("pushover.cfg")
+    apiToken = configuration.get("Pushover", "api_token")
+    userKey = configuration.get("Pushover", "user_key")
+
+    title = "Download complete"
+    message = torrentName
+
+    cx = httplib.HTTPSConnection("api.pushover.net:443")
+    cx.request(
+        "POST",
+        "/1/messages",
+        urllib.urlencode({
+            "token": apiToken,
+            "user": userKey,
+            "title": title,
+            "message": message
+        }),
+        {"Content-type": "application/x-www-form-urlencoded"}
+    )
+
+
 if 'TR_TORRENT_NAME' not in os.environ:
     sys.exit("Torrent name not set")
 else:
     push(os.environ['TR_TORRENT_NAME'])
-	
