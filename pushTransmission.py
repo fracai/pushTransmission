3bb0c8af75c6dc31cee30aa1919ad35c3c31b721
Arno Hautala <arno@alum.wpi.edu>
2016-03-22 21:53:13 -0400

    more exception handling, read config using 'with open'

--- pushTransmission.py
+++ pushTransmission.py
@@ -8,26 +8,27 @@ import os
 
 
 def read_config(config):
-    configuration = ConfigParser.ConfigParser()
     config_path = os.path.join(os.path.dirname(sys.argv[0]), config)
 
     if not os.path.isfile(config_path):
         sys.exit("Config file not found")
 
     try:
-        fp = open(config_path, "r")
-        configuration.readfp(fp)
-        fp.close()
+        with open(config_path, 'r') as config_fp:
+            configuration = ConfigParser.ConfigParser()
+            configuration.readfp(config_fp)
+            return configuration
     except IOError:
         sys.exit("Config file not readable")
-
-    return configuration
 
 
 def push(torrent_name):
     configuration = read_config("pushover.cfg")
-    api_token = configuration.get("Pushover", "api_token")
-    user_key = configuration.get("Pushover", "user_key")
+    try:
+        api_token = configuration.get("Pushover", "api_token")
+        user_key = configuration.get("Pushover", "user_key")
+    except ConfigParser.NoSectionError, ConfigParser.NoOptionError:
+        sys.exit("Config File missing tokens")
 
     app = chump.Application(api_token)
     user = app.get_user(user_key)
